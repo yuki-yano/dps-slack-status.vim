afb763a3a36a722314818ca42b2de30c2052c3b3
yuki_yano <yuki.ycino@gmail.com>
2021-04-09 22:23:51 +0900

    Initial commit

---
+++ denops/slack-status/mod.ts
@@ -0,0 +1,57 @@
+import { start } from "https://deno.land/x/denops_std@v0.4/mod.ts";
+
+let token: string | undefined = undefined;
+
+const changeStatus = async (...args: Array<unknown>) => {
+  const status = (args as Array<string>).join("");
+
+  if (status === "") {
+    console.log("[SlackStatus] Status is empty");
+    return;
+  }
+
+  const profile = {
+    profile: {
+      status_text: status,
+      status_expiration: Math.floor(Date.now() / 1000) + 1200,
+    },
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(profile),
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "Authorization": `Bearer ${token}`,
+    },
+  };
+
+  await fetch("https://slack.com/api/users.profile.set", options);
+};
+
+start(async (vim) => {
+  token = Deno.env.get("SLACK_STATUS_TOKEN");
+
+  if (token == null || token === "") {
+    console.log("[SlackStatus] Token is not set");
+    return;
+  }
+
+  const messageContent = await vim.g.get(
+    "slack_status_message",
+    '["I\'m coding ", &filetype]',
+  ) as string;
+
+  vim.register({ changeStatus });
+
+  await vim.execute(
+    `command! SlackStatusWrite call denops#notify("${vim.name}", "changeStatus", ${messageContent})`,
+  );
+
+  await vim.execute(`
+augroup slack_status
+  autocmd!
+  autocmd BufWinEnter,WinEnter * SlackStatusWrite
+augroup END
+    `);
+});
