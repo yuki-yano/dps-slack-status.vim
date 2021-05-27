ef293ce085fb6ab261cc49516576415414b38f88
yuki_yano <yuki.ycino@gmail.com>
2021-05-27 10:22:28 +0900

    Add User event

--- denops/slack-status/app.ts
+++ denops/slack-status/app.ts
@@ -55,6 +55,10 @@ main(async ({ vim }) => {
     `command! SlackStatusWrite call denops#notify("${vim.name}", "changeStatus", ${messageContent})`,
   );
 
+  await vim.execute(
+    `silent doautocmd User DenopsSlackStatusReady`,
+  );
+
   await vim.execute(`
 augroup slack_status
   autocmd!
